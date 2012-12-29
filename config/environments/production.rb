ad064c1a10d8bf68787fcff1f9b922f3c981ccf3
Jon Hezghia <jhezghia@gmail.com>
2012-12-29 03:39:12 -0500

    removed index.html

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Wiwik::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.default_url_options = { :host => 'herokuapp.com' }
   # Code is not reloaded between requests
   config.cache_classes = true
 
