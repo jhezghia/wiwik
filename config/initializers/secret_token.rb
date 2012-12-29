d829f57ad1201394473c1e98b5c5c830c834743f
Jon Hezghia <jhezghia@gmail.com>
2012-12-29 00:58:29 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wiwik::Application.config.secret_token = 'd9d68f8d3e3e1a8eca751dfae12f73a20a486d274e8a7dc8a2a50eb0c7e6505a9eb3d8bac63779ea4916ffdda3472dd041f54949f4e19c09baf483b73c7246b5'
